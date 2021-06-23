86d463aec3ab32d8aadebf83d739752d25d9a1ba
Tasha McAllister <tashabmcallister@gmail.com>
2021-06-23 13:32:02 -0500

    Icons, Favorites, and Comments

---
+++ components/ContactComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card
+                   wrapperStyle={{margin: 20}}
+                   title='Contact Information'
+                >
+                    <Text
+                        style={{marginBottom: 10}}
+                    >
+                        1 Nucamp Way{'\n'}
+                        Seattle, WA 98001{'\n'}
+                        U.S.A.{'\n'}
+                        {'\n'}
+                        Phone: 1-206-555-1234{'\n'}
+                        Email: campsites@nucamp.co
+                    </Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact;
