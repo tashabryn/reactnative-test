be481941fec5ed9a0d8b83fa56bcba701720d3f0
Tasha McAllister <tashabmcallister@gmail.com>
2021-07-10 15:13:09 -0500

    Week 4 Assignment - need help

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -332,17 +332,20 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
+        this.showNetInfo();
+
+        //this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+        //    this.handleConnectivityChange(connectionInfo);
+        //});
+    }
+
+    async showNetInfo() {
+        await NetInfo.fetch().then(connectionInfo => {
             (Platform.OS === 'ios')
                 ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
                 : ToastAndroid.show('Initial Network Connectivity Type: ' +
                     connectionInfo.type, ToastAndroid.LONG);
-        });
-
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
-    }
+    })};
 
     componentWillUnmount() {
         this.unsubscribeNetInfo();
@@ -365,8 +368,8 @@ class Main extends Component {
                 break;
         }
         (Platform.OS === 'ios')
-        ? Alert.alert('Connection change:', connectionMsg)
-        : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+            ? Alert.alert('Connection change:', connectionMsg)
+            : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
     }
 
     render() {
